6edf938c1e2b37c0f45da76a8da5048e0acfd46b
G1oZ4m <162560734+G1oZ4m@users.noreply.github.com>
2025-01-13 13:55:28 +0100

    Modification of calculate function
    Adding correct working code with browser. In that : 
    1.  Receiving expressions from text field.
    2.  Sending expression.
    3.  Displaying results on webside.

--- web_side/js/script.js
+++ web_side/js/script.js
@@ -9,22 +9,17 @@ function clearDisplay(){
     display.value = "";
 }
 
-function calculate(){
-    display.value = eval(display.value);
-}
-
 function calculate() {
-    const expression = document.getElementById("display").value;
+    const expression = document.getElementById("display").value; // Receiving expression from text field
 
-    fetch("http://localhost:8000/calculate", {
+    fetch("http://localhost:8001/calculate", {
         method: "POST",
-        headers: { "Content-Type": "text/plain" },
-        body: expression,
+        headers: {"Content-Type": "text/plain"},
+        body: expression, // Sending expression
     })
     .then(response => response.text())
     .then(result => {
-        document.getElementById("display").value = result;
+        document.getElementById("display").value = result; // Showing result on website
     })
-    .catch(error => console.error("Error:", error));
+    .catch(error => console.error("Error: ", error));
 }
-
